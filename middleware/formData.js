c3ff15a89054e88c70aa24748fa4d5243aff1469
wafiyana-ibr <wafiyana00@gmail.com>
2025-05-12 10:02:37 +0700

    Delete middleware directory

--- middleware/formData.js
+++
@@ -1,9 +0,0 @@
-const formDataWafi = require('express-form-data');
-const osWafi = require('os');
-
-const formDataOptionsWafi = {
-    uploadDir: osWafi.tmpdir(),
-    autoClean: true,
-};
-
-module.exports = { formDataWafi, formDataOptionsWafi };
